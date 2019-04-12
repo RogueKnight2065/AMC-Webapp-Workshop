4d97736421cb0b6794d42b068de40b94816ec7ae
Matt Anuszkiewicz <matta2z17@gmail.com>
2019-04-12 16:49:39 -0400

    First push for web app

---
+++ src/Components/Header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+import About from "./Components/About"
+class App extends Component {
+    render() {
+        return (
+
+            <div>
+            <About/>
+            </div>
+    );
+    }
+}
+
+export default App;
